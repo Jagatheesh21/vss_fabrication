7e73e0deaf13b484f0336e05e6936800acf3f53a
Jagatheesh21 <jagatheesh21@gmail.com>
2022-10-28 20:38:44 +0530

    First Commit

--- resources/views/store/store_issue_rm_entry.blade.php
+++ resources/views/store/store_issue_rm_entry.blade.php
@@ -16,7 +16,6 @@
   <strong>Error!</strong> {{session('error')}}.
   <button type="button" class="btn-close" data-coreui-dismiss="alert" aria-label="Close"></button>
 </div>
-
 @endif
   
     <div class="card">
